1352c80c44a81acdf92e4346c8ce04aef85472fd
ConradoChiesa <conradochiesa@gmail.com>
2024-12-18 00:47:28 -0300

    First production commit

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -26,8 +26,8 @@ export default {
   assets: {
     'index.csr.html': {size: 5440, hash: 'b8edb9d9310526b790e7ed7bc9da1915cb0eebabb9fc7d746329f18177ec12c9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 1195, hash: 'ae23823414f9090fee0003356352aac9f772aec1cab61dc9259e2d68eb4b8027', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'home/index.html': {size: 17351, hash: '7e83ac60fb0c921feb63bf61b3a10d555ffa3b5671500d493aab4a867605d9b0', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
     'about/index.html': {size: 19398, hash: '0735a6abbb600fc9f587da7ca0b1c242b803f2d46162c2d5d669c9ea3dca7245', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
+    'home/index.html': {size: 17351, hash: '7e83ac60fb0c921feb63bf61b3a10d555ffa3b5671500d493aab4a867605d9b0', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
     'contact/index.html': {size: 19728, hash: '82b64d9609e29b03ea5aa959da717ca57587f43a9ff0ac3bd843fb2329edff5d', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
     'styles-R37GORVW.css': {size: 226826, hash: 'd0uUHn5AnKw', text: () => import('./assets-chunks/styles-R37GORVW_css.mjs').then(m => m.default)}
   },
